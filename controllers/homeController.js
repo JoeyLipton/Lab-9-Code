7f1eb3165edc337d94939b0d53d12adfa5a3fb9c
Joey Lipton <harmansohi16@gmail.com>
2022-04-02 21:03:25 -0400

    ye 3

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,2 +1,21 @@
 const path = require("path");
 
+exports.sendReqParam = (req, res) => {
+    switch (req.params.page) {
+        
+        case 'arch' :
+            res.sendFile(path.join(__dirname, "../public/images/arch.png"));
+            break;
+        case 'deb' :
+            res.sendFile(path.join(__dirname, "../public/images/deb.png"));
+            break;
+        case 'rhel' :
+            res.sendFile(path.join(__dirname, "../public/images/rhel.png"));
+            break;
+        default :
+            res.render("index", {
+                name: "home"
+            });
+            break;
+    };
+};
--- index.js
+++ index.js
@@ -2,22 +2,29 @@ const port = 3000;
 const express = require("express"),
     app = express(),
     homeController = require("./controllers/homeController")
+const expressLayouts = require('express-ejs-layouts')
 
-app.set("port", process.env.PORT || port);
+app.set("port", port);
 app.set("view engine", "ejs");
 app.use(express.static("public"));
-
 app.use(express.urlencoded({ extended: false }));
-
+app.use(expressLayouts)
 app.use(express.json());
 
 app.use((req, res, next ) => {
     next();
 });
 
+app.get("", (req,res) => {
+    res.render("index");
+});
+
 app.get("/home", (req,res) => {
     res.render("home");
 });
 
-app.get("/images/:images", homeController.paramsimage);
-app.listen(app.get(port));
+app.get("/images/:images", (req,res) => {
+    res.render("index");
+});
+
+app.listen(port);
